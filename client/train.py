fe97c59fa0f981f53e074204fac4311a86951be5
saadiabadi <saadiabadi@gmail.com>
2022-04-30 18:48:35 +0200

    evaluation metrics

--- client/train.py
+++ client/train.py
@@ -23,28 +23,29 @@ def np_to_weights(weights_np):
 
 def train(model, loss, optimizer, data, settings):
     print("-- RUNNING TRAINING --", flush=True)
-    trainset = read_data(data)
+
 
     # We are caching the partition in the container home dir so that
     # the same training subset is used for each iteration for a client. 
-    # try:
-    #     with open('/tmp/local_dataset/trainset.p','rb') as fh:
-    #         trainset = pickle.loads(fh.read())
-    #     print("load local trainset")
-    #
-    # except:
-    #     trainset = read_data(trainset=True, nr_examples=settings['training_samples'],  data_path='../data/nmrshift.npz')
-    #     print("sample new local trainset")
-    #
-    #     try:
-    #         if not os.path.isdir('/tmp/local_dataset'):
-    #             os.mkdir('/tmp/local_dataset')
-    #
-    #         with open('/tmp/local_dataset/trainset.p','wb') as fh:
-    #             fh.write(pickle.dumps(trainset))
-    #
-    #     except:
-    #         pass
+    try:
+        with open('/tmp/local_dataset/trainset.p','rb') as fh:
+            trainset = pickle.loads(fh.read())
+        print("load local trainset")
+
+    except:
+        trainset = read_data(data)
+        # trainset = read_data(trainset=True, nr_examples=settings['training_samples'],  data_path='../data/nmrshift.npz')
+        print("sample new local trainset")
+
+        try:
+            if not os.path.isdir('/tmp/local_dataset'):
+                os.mkdir('/tmp/local_dataset')
+
+            with open('/tmp/local_dataset/trainset.p','wb') as fh:
+                fh.write(pickle.dumps(trainset))
+
+        except:
+            pass
 
     print('=========================SADI ======================================= Model Load')
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=settings['batch_size'], shuffle=True)
--- client/validate.py
+++ client/validate.py
@@ -4,6 +4,7 @@ import json
 import yaml
 import torch
 import collections
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 
 def np_to_weights(weights_np):
@@ -12,44 +13,71 @@ def np_to_weights(weights_np):
         weights[w] = torch.tensor(weights_np[w])
     return weights
 
-def validate(model, data,loss, settings):
+def validate(model, data, settings):
     print("-- RUNNING VALIDATION --", flush=True)
     # The data, split between train and test sets. We are caching the partition in 
     # the container home dir so that the same data subset is used for 
     # each iteration.
 
+    def r2_loss(output, target):
+        target_mean = torch.mean(target)
+        ss_tot = torch.sum((target - target_mean) ** 2)
+        ss_res = torch.sum((target - output) ** 2)
+        r2 = 1 - ss_res / ss_tot
+        return r2
+
     def evaluate(model, loss, dataloader):
         model.eval()
         train_loss = 0
+        train_loss1 = 0
+        train_correct = 0
+        batch_size = 32
         with torch.no_grad():
             for x, y in dataloader:
     
-                batch_size = x.shape[0]
-                x = torch.squeeze(x, 1)
-                x_float = torch.from_numpy(x.float().numpy())
+                batch_size1 = x.shape[0]
+                print('---------------------------------------------------------------------------')
+                print('                     BATCH SIZE =  %g                         '%batch_size1 )
+                print('---------------------------------------------------------------------------')
+                # x = torch.squeeze(x, 1)
+                x1=x.float().numpy()
+                x_float = torch.from_numpy(x1)
 
                 output = model.forward(x_float)
+                train_loss1 += batch_size * loss(output, y).item()
                 
                 input = torch.zeros((batch_size, 128), dtype=torch.float32)
                 input_mask = torch.zeros((batch_size, 128), dtype=torch.int32)
-                for i, row in enumerate(x):
+                for i, row in enumerate(x1):
                     input_mask[i, int(torch.FloatTensor(row)[70401].item())] = 1
                     input[i, int(torch.FloatTensor(row)[70401].item())] = float(y[i].item())
 
                 train_loss += batch_size * loss(output, input, input_mask).item()
 
-                # pred = output.argmax(dim=1, keepdim=True)
-                # train_correct += pred.eq(y.view_as(pred)).sum().item()
+
+                pred = output.argmax(dim=1, keepdim=True)
+                train_correct += pred.eq(y.view_as(pred)).sum().item()
+
+
+                r2 = r2_loss(output, y)
+                r2.backward()
+                mse = mean_squared_error(y, output)
+                r_square = r2_score(y, output)
+                mae=mean_absolute_error(y, output)
+
             train_loss /= batch_size
+            train_loss /= len(dataloader.dataset)
             train_acc = train_correct / len(dataloader.dataset)
-        return float(train_loss), float(train_acc)
+
+        return float(train_loss), float(train_acc), float(r2), float(mse), float(r_square), float(mae)
 
     # # Load train data
     # try:
     #     with open('/tmp/local_dataset/trainset.pyb', 'rb') as fh:
     #         trainset = pickle.loads(fh.read())
     # except:
-    #     trainset = read_data(trainset=True, nr_examples=settings['training_samples'], data_path='../data/nmrshift.npz')
+    #     # trainset = read_data(trainset=True, nr_examples=settings['training_samples'], data_path='../data/nmrshift.npz')
+    #     testset = read_data(data)
     #     try:
     #         if not os.path.isdir('/tmp/local_dataset'):
     #             os.mkdir('/tmp/local_dataset')
@@ -57,28 +85,29 @@ def validate(model, data,loss, settings):
     #             fh.write(pickle.dumps(trainset))
     #     except:
     #         pass
-    #
-    # # Load test data
-    # try:
-    #     with open('/tmp/local_dataset/testset.pyb', 'rb') as fh:
-    #         testset = pickle.loads(fh.read())
-    # except:
-    #     testset = read_data(trainset=False, nr_examples=settings['test_samples'],  data_path='../data/nmrshift.npz')
-    #     try:
-    #         if not os.path.isdir('/tmp/local_dataset'):
-    #             os.mkdir('/tmp/local_dataset')
-    #         with open('/tmp/local_dataset/trainset.pyb', 'wb') as fh:
-    #             fh.write(pickle.dumps(testset))
-    #     except:
-    #         pass
 
-    testset = read_data(data)
+    # Load test data
+    try:
+        with open('/tmp/local_dataset/testset.pyb', 'rb') as fh:
+            testset = pickle.loads(fh.read())
+    except:
+        testset = read_data(data)
+        # testset = read_data(trainset=False, nr_examples=settings['test_samples'],  data_path='../data/nmrshift.npz')
+        try:
+            if not os.path.isdir('/tmp/local_dataset'):
+                os.mkdir('/tmp/local_dataset')
+            with open('/tmp/local_dataset/trainset.pyb', 'wb') as fh:
+                fh.write(pickle.dumps(testset))
+        except:
+            pass
+
+
     # train_loader = torch.utils.data.DataLoader(trainset, batch_size=settings['batch_size'], shuffle=True)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=settings['batch_size'], shuffle=True)
 
     try:
         # training_loss, training_acc = evaluate(model, loss, train_loader)
-        test_loss, test_acc = evaluate(model, loss, test_loader)
+        test_loss, test_acc, r2_, mse1,r2_s, mae1 = evaluate(model, loss, test_loader)
 
     except Exception as e:
         print("failed to validate the model {}".format(e), flush=True)
@@ -90,6 +119,10 @@ def validate(model, data,loss, settings):
                 # "training_accuracy": training_acc,
                 "test_loss": test_loss,
                 "test_accuracy": test_acc,
+                "R2": r2_,
+                "MSE": mse1,
+                "R2_score": r2_s,
+                "MAE": mae1,
             }
 
     print("-- VALIDATION COMPLETE! --", flush=True)
@@ -110,7 +143,11 @@ if __name__ == '__main__':
     print('=========================SADI ======================================= Model Validation')
     model.load_state_dict(np_to_weights(helper.load_model(sys.argv[1])))
 
-    report = validate(model,'../data/test.csv',loss, settings)
+    report = validate(model,'../data/test.csv', settings)
+    print('=========================Report ======================================= ')
+    print(report)
+
+    print('================================================================ ')
 
     with open(sys.argv[2], "w") as fh:
         fh.write(json.dumps(report))
